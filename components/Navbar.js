d0f1a418c81f58dc13fbc0c0b6a6c0d82542a23e
excel <excelyynxl@gmail.com>
2026-01-15 09:04:36 +0800

    nav bar only have (home, shorten, try now and github), please expand the (about and contact us) when u are free enxt time!

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,28 +15,30 @@ const Navbar = () => {
                     Home
                 </li>
             </Link>
-            <Link href="/about" className="block px-2 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:text-white hover:bg-gray-700">
+            {/* <Link href="/about" className="block px-2 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:text-white hover:bg-gray-700">
                 <li>
                     About
                 </li>
-            </Link>
+            </Link> */}
             <Link href="/shorten" className="block px-2 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:text-white hover:bg-gray-700">
                 <li>
                     Shorten
                 </li>
             </Link>
-            <Link href="/contact" className="block px-2 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:text-white hover:bg-gray-700">
+            {/* <Link href="/contact" className="block px-2 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:text-white hover:bg-gray-700">
                 <li>
                     Contact Us
                 </li>
-            </Link>
+            </Link> */}
 
             <li className='flex gap-3'>
                 <Link href="/shorten" >
                     <button className='bg-purple-500 rounded-lg shadow-lg p-3 py-1 font-bold hover:bg-purple-600'>Try Now</button>
                 </Link>
-                <Link href="/github">
-                    <button className='bg-purple-800 rounded-lg shadow-lg p-3 py-1 font-bold hover:bg-purple-900'>GitHub</button>
+                <Link href="https://github.com/excelyynxl-a11y" target="_blank">
+                    <button className='bg-purple-800 rounded-lg shadow-lg p-3 py-1 font-bold hover:bg-purple-900'>
+                        GitHub
+                    </button>
                 </Link>
             </li>
         </ul>
